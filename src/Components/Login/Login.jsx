9bc7c6e3ef98acee2830520ea675eab851b977b7
Avik Roy <somavik99@gmail.com>
2023-04-24 12:13:44 +0530

    LogInSignUp commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import "./Login.css";
+import { ToastContainer, toast } from "react-toastify";
+
+const Login = () => {
+  const [formSubmit, setFormSubmit] = useState({
+    userName: "",
+    pass: "",
+  });
+
+  const FormChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormSubmit(() => {
+      return { ...formSubmit, [name]: value };
+    });
+  };
+
+  const emailRegex = new RegExp(
+    "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$"
+  );
+  const passRegex = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/
+  );
+
+  const FormSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const LoginForm = () => {
+    const { userName, pass } = formSubmit;
+
+    if (!userName.match(emailRegex)) {
+      toast.error(" ", {
+        position: "top-center",
+      });
+    } else if (!pass.match(passRegex)) {
+      toast.error("", {
+        position: "top-center",
+      });
+    } else {
+      console.log("LoggedIn", userName, pass);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="login-container">
+        <h1 className="h1" >Login </h1>
+        <form action="" className="login-form" onSubmit={FormSubmitHandler}>
+          <div className="user">
+            <label
+              htmlFor=""
+              style={{ margin: "3px", padding: "5px", fontSize: "20px" }}
+            >
+              UserName or Email
+            </label>
+            <div>
+              <input
+                type="text"
+                value={formSubmit.userName}
+                name="userName"
+                placeholder="User..."
+                onChange={FormChangeHandler}
+                style={{ margin: "3px", padding: "5px", height: "50px" }}
+              />
+              <ToastContainer style={{ height: "20px", width: "20px" }} />
+            </div>
+          </div>
+
+          <div className="password">
+            <label
+              htmlFor=""
+              style={{ margin: "3px", padding: "5px", fontSize: "20px" }}
+            >
+              Password
+            </label>
+            <div>
+              <input
+                type="password"
+                name="pass"
+                value={formSubmit.pass}
+                placeholder="password..."
+                onChange={FormChangeHandler}
+                style={{ margin: "3px", padding: "5px", height: "50px" }}
+              />
+              <ToastContainer style={{ height: "20px", width: "20px" }} />
+            </div>
+          </div>
+
+          <div className="btnSubmit">
+            <button className="btn" type="submit" onClick={LoginForm}>
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
